a5968c1a61d057f372f39af0d73360879573cbb6
ALLAHNANA <usmanallahnana@gmail.com>
2021-11-08 13:55:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//Init Github
+const github = new  Github
+
+//Init UI
+
+const ui = new UI
+
+
+//Search Input
+const searchUser = document.getElementById('searchUser');
+
+//Search Input event listeners
+searchUser.addEventListener('keyup', (e) => {
+
+//Get inpit text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+
+    //Make http call
+        github.getUser(userText)
+         .then(data => {
+
+            // console.log(data)
+             if(data.profile.message === 'Not Found'){
+                 //Show Alert
+                 ui.showAlert('User not found', 'alert alert-danger');
+             }else{
+                 //Show Profile
+                 ui.showProfile(data.profile );
+
+                 //Show repos
+                 ui.showRepos(data.repos);
+             }
+         })
+
+    }else{
+        //clear Profile
+        ui.clearProfile();
+    }
+   
+
+});
---
+++ ui.js
@@ -0,0 +1,110 @@
+class UI{
+    constructor(){
+       this.profile = document.getElementById('profile') ;
+        
+    }
+//DISPLAY PROFILE IN UI
+    showProfile(user){
+
+        // console.log(user)
+
+        this.profile.innerHTML = `
+
+        <div class="card card-body mb-3">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatar_url}">
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+          </div>
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+            <li class="list-group-item">Name: ${user.name}</li>
+            <li class="list-group-item">Bio: ${user.bio}</li>
+            <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h3 class="page-heading mb-3">Latest Repos</h3>
+      <div id="repos"></div>
+     
+        `;
+
+    }
+
+    //Show user repo
+    showRepos(repos){
+      let output = ''
+
+      repos.forEach((repo) =>{
+
+        output += `
+        <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+          <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+          <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+          <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+          </div>
+        </div>
+      </div>
+        `;
+      });
+
+      //Output repos
+      document.getElementById('repos').innerHTML = output;
+
+    }
+    //CLEAR PROFILE
+    clearProfile(){
+        this.profile.innerHTML = '';
+    }
+    // SHOW ALERT MESSAGE
+    showAlert(message, className){ 
+        //clear any alert remaining
+        this.clearAlert();
+
+
+
+        //create div
+        const div = document.createElement('div');
+        //add classes
+        div.className = className;
+        //add text
+        div.appendChild(document.createTextNode(message));
+        //get parrent
+        const container = document.querySelector('.searchContainer');
+        //get search box
+        const search = document.querySelector('.search');
+        //insert alert
+        container.insertBefore(div,search);
+
+        //timeout after 3sec
+        setTimeout(() =>{
+            this.clearAlert()
+        },3000);
+
+
+
+    }
+
+    //CLEAR ALERT MESSAGE
+    clearAlert(){
+        const currentAlert = document.querySelector('.alert')
+
+        if(currentAlert){
+            currentAlert.remove();
+        }
+    }
+}
